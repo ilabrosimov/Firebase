d5bdaf26c32b475ae6747218365ad642d75bd42d
ilabrosimov <ilabrosimov@gmail.com>
2020-05-24 21:16:14 +0300

    Add interface Add outlets and actions

---
+++ Firebase/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by ilabrosimov on 24.05.2020.
+//  Copyright © 2020 ilabrosimov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextView: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var warnLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+}
+
