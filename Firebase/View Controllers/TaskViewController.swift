ca1a62f4536b1c7d278ce0a2f8f45af9fa4f427a
ilabrosimov <ilabrosimov@gmail.com>
2020-05-25 12:27:40 +0300

    Отредактированы кнопки. Цвет, шрифт, ограничения

--- Firebase/View Controllers/TaskViewController.swift
+++ Firebase/View Controllers/TaskViewController.swift
@@ -18,6 +18,7 @@ class TaskViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        cell.textLabel?.text = String (indexPath.row)
         return cell
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
